a517486582be067fc6335bf933de7ede903c59a7
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-09-21 17:22:17 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -17,4 +17,11 @@ class InteractiveRecord
     end
     column_names.compact
   end
+
+  def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end
+  end
+  
 end
