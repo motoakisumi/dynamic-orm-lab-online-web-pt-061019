bf553befc9acdb22d7105f6fae8b3c3d01d5d252
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-09-21 17:55:18 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -57,7 +57,7 @@ class InteractiveRecord
 
     sql = <<-SQL
       SELECT * FROM #{self.table_name}
-      WHERE #{self.column_name} = ?
+      WHERE #{column_name} = ?
       SQL
 
     DB[:conn].execute(sql, value_name);
