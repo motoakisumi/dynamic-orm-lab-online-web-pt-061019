258a51e992dae6bcf25678c314eeba97f0746bea
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-09-21 17:52:25 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -51,10 +51,16 @@ class InteractiveRecord
     DB[:conn].execute(sql, name)
   end
 
-  def self.find_by(options={})
-    options.each do |property, value|
-      sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
-      DB[:conn].execute(sql, "#{property}=", value)
-    end
+  def self.find_by(attribute)
+    column_name = attribute.keys[0].to_s
+    value_name = attribute.values[0]
+
+    sql = <<-SQL
+      SELECT * FROM #{table_name}
+      WHERE #{column_name} = ?
+      SQL
+
+    DB[:conn].execute(sql, value_name);
   end
+  
 end
